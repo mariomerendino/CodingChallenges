1df62bd5e39b99dbb7cdf47191916f5d213c19d3
mmerendino@fordham.edu <mmerendino@fordham.edu>
2017-08-17 18:29:37 +0200

    Cleanup.

--- ch1/matrix.java
+++ ch1/matrix.java
@@ -16,27 +16,31 @@ public class matrix{
     }
 
     public void replaceZeros(){
+
         int[] row = new int[matrix.length];
         int[] col = new int[matrix[0].length];
         for(int i = 0; i < matrix.length; i++){
             for(int j = 0; j < matrix[i].length; j++){
+                //if there is a zero, that entire row and column must be
+                //replaced with zeros
                 if(matrix[i][j] == 0){
                     row[i] = 1;
                     col[j] = 1;
                 }
             }
         }
+
         for(int i =0; i < row.length; i++){
             if(row[i] == 1){
                 for(int j = 0; j < matrix.length; j++){
-                    matrix[i][j] = 0;
+                    matrix[j][i] = 0;
                 }
             }
         }
-        for(int i =0; i < row.length; i++){
+        for(int i =0; i < col.length; i++){
             if(col[i] == 1){
-                for(int j = 0; j < matrix.length; j++){
-                    matrix[j][i] = 0;
+                for(int j = 0; j < matrix[0].length; j++){
+                    matrix[i][j] = 0;
                 }
             }
         }
@@ -44,6 +48,7 @@ public class matrix{
     }
 
 
+    //fill matrix with random values
     public void fillMatrix(){
         for(int i = 0; i < matrix.length; i++){
             for(int j = 0; j < matrix[i].length; j++){
