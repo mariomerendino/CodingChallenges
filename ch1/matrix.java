d3e4cceb7eaac1d88394493088ffe177013e71b5
mmerendino@fordham.edu <mmerendino@fordham.edu>
2017-08-17 14:58:37 +0200

    fixed Matrix, runtime is now O(n^2)

--- ch1/matrix.java
+++ ch1/matrix.java
@@ -16,22 +16,31 @@ public class matrix{
     }
 
     public void replaceZeros(){
-        int[][] x = new int[10][10];
-        x = matrix;
+        int[] row = new int[matrix.length];
+        int[] col = new int[matrix[0].length];
         for(int i = 0; i < matrix.length; i++){
             for(int j = 0; j < matrix[i].length; j++){
                 if(matrix[i][j] == 0){
-                    for(int y = 0; y < matrix.length; y++)
-                        x[i][y] = 0;
-                    for(int y = 0; y < matrix.length; y++)
-                        x[y][j] = 0;
-                }
-                else{
-                    x[i][j] = matrix[i][j];
+                    row[i] = 1;
+                    col[j] = 1;
                 }
             }
         }
-        matrix = x;
+        for(int i =0; i < row.length; i++){
+            if(row[i] == 1){
+                for(int j = 0; j < matrix.length; j++){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        for(int i =0; i < row.length; i++){
+            if(col[i] == 1){
+                for(int j = 0; j < matrix.length; j++){
+                    matrix[j][i] = 0;
+                }
+            }
+        }
+
     }
 
 
