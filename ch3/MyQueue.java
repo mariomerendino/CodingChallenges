ba544a948c512f1fb4151ce0c6ddebba3a29cc84
mmerendino@fordham.edu <mmerendino@fordham.edu>
2017-08-31 15:17:16 -0400

    Queue with 2 Stacks

---
+++ ch3/MyQueue.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+
+public class MyQueue{
+    public Stack<String> s1 = new Stack<String>();
+    public Stack<String> s2 = new Stack<String>();
+    
+
+    public static void main(String[] args){
+        MyQueue q = new MyQueue();
+        int input = 1;
+        Scanner sc = new Scanner(System.in);
+        while(input != 0){
+            System.out.println("1.Print Queue\n2.Add\n3.Remove\n4.Peek\n0.Exit\n:");
+            input = sc.nextInt();
+            if(input == 1){
+                q.printQueue();
+            }
+            if(input == 2){
+                System.out.println("What would you like to add?\n");
+                q.push(sc.next());
+            }
+            if(input == 3){
+                System.out.println(q.remove() + " Removed");                
+            }
+            if(input == 4){
+                System.out.println(q.peek() + " is up next");                                
+            }
+
+        }
+    }
+    public MyQueue(){
+
+    }
+    public void push(String val){
+        s1.push(val);
+    }
+    public String remove(){
+        while(!s1.isEmpty()){
+            s2.push(s1.pop());
+        }
+        String ans = s2.pop();
+        while(!s2.isEmpty()){
+            s1.push(s2.pop());
+        }
+        return ans;
+    }
+    public String peek(){
+        while(!s1.isEmpty()){
+            s2.push(s1.pop());
+        }
+        String ans = s2.peek();
+        while(!s2.isEmpty()){
+            s1.push(s2.pop());
+        }
+        return ans;
+    }
+    public void printQueue(){
+        System.out.println("\nMy Queue:");
+        while(!s1.isEmpty()){
+            System.out.print(s1.peek() + " ");
+            s2.push(s1.pop());
+        }
+        System.out.println();
+        while(!s2.isEmpty()){
+            s1.push(s2.pop());
+        }
+    }
+}
--- ch9/mergetwo.java
+++ ch9/mergetwo.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class mergetwo{
     public static void main(String[] args){
 
-        int[] a = new int[5];
+        int[] a = new int[10];
         int[] b = new int[5];
         a[0] = 1;
         for(int i = 1; i < 5; i++){
@@ -13,33 +13,38 @@ public class mergetwo{
         for(int i = 1; i < 5; i++){
             b[i] = i + 1;
         }
+        System.out.println("Print Array A:");
         printArr(a);
-        //rightShift(a, 10, 3);
+        System.out.println("Print Array B:");
         printArr(b);
         merge(a,5,b, 5);
-        //printArr(a);
+        System.out.println("Print Merged:");        
+        printArr(a);
 
 
     }
     //Merge two Sorted Arrays, arr1 has a large enough Buffer for both arrays
     public static void merge(int arr1[], int l1,  int arr2[], int l2){
-        int[] ans = new int[l1 + l2 - 2];
-        int i = 0;
         int j = 0;
-        for(int x = 0; x < ans.length; x++){
+        int temp = 0;
+        for(int i = 0; i < arr1.length; i++){
             if(arr1[i] >= arr2[j]){
-                ans[x] = arr2[j];
+                //System.out.println("Index: " + Integer.toString(i)+ " " + Integer.toString(arr1[i]));
+                //System.out.println("Index: " + Integer.toString(j)+ " " + Integer.toString(arr2[j]));
+                temp = arr1[i];
+                for(int x = arr1.length-1; x < i+1; x--){
+                    arr1[x] = arr1[x-1];
+                }
+                arr1[i] = arr2[j];                
+                arr1[i+1] = temp;
+                i++;                                
                 j++;
-            }
-            else{
-                ans[x] = arr1[i];
-                i++;
+                printArr(arr1);
             }
         }
-        printArr(ans);
     }
     public static void rightShift(int[] arr, int leng, int index){
-        int temp = arr[index];
+        //int temp = arr[index];
         for(int i = index; i < leng-2; i++){
 
         }
